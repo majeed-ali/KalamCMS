ce34339c5650afb0ec8f9a58bfff08cf744e8ea2
majeed-ali <52486635+majeed-ali@users.noreply.github.com>
2022-03-13 16:31:26 +0400

    Update 404.php
    URL Added in the Views 404 Page

--- views/404.php
+++ views/404.php
@@ -3,7 +3,7 @@
 ?>
 <section id="404">
     <div class="col-md-12">
-        <img src="image/404.gif" alt="404 Error" class="mx-auto d-block">
+        <img src="http://localhost/image/404.gif" alt="404 Error" class="mx-auto d-block">
         <br>
         <p class="f-title text-center">WE ARE SORRY, PAGE NOT FOUND</p>
         <p class="text-center"><a href="/" class="text-center text-info">BACK TO HOME</a></p>
